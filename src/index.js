0c8522fe3f0cb9fae39d9fb077d5e7f69f5aa99b
Rey Karel <karel.reiy@gmail.com>
2023-04-06 20:24:28 +0300

    fixed api

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import 'react-tooltip/dist/react-tooltip.css'
-require('dotenv').config()
+
+// This line sets the path to the root of your project
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
