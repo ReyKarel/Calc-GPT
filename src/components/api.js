d05ff3a1f0599f492816490b4137d159646b8a3f
Rey Karel <karel.reiy@gmail.com>
2023-04-07 16:30:48 +0300

    fixed api again

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setResponse } from "../store/slices/gptSlice";
-
+require('dotenv').config()
 
 const API_KEY = process.env.REACT_APP_OPENAI_API_KEY
+
 export async function processChatGPTRequest(prompt, result) {
     const systemMessage = {
         role: 'system',
@@ -32,29 +33,3 @@ export async function processChatGPTRequest(prompt, result) {
 }
 
 
-// export async function processChatGPTRequest() {
-//     const systemMessage = {
-//         role: 'system',
-//         content: prompt
-//     };
-//     const userMessage = {
-//         role: 'user',
-//         content: result.toString()
-//     };
-//     const apiRequestBody = {
-//         'model': 'gpt-3.5-turbo',
-//         'messages': [systemMessage, userMessage]
-//     };
-//     await fetch('https://api.openai.com/v1/chat/completions', {
-//         method: 'POST',
-//         headers: {
-//             'Authorization': 'Bearer ' + API_KEY,
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(apiRequestBody)
-//     }).then((data) => {
-//         return data.json();
-//     }).then((data) => {
-//         dispatch(setResponse(data.choices[0].message.content));
-//     });
-// }
