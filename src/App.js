e0d854d4a55146c10f4cf8af0e46358b946ab86e
ReyKarel <karel.reiy@gmail.com>
2023-04-27 21:19:32 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -7,36 +7,36 @@ import Info from "./components/Info";
 const App = () => {
     return (
         <div className='box'>
-            <div className="row-item">
-                <ChatPrompt />
-            </div>
-            <div className="row-item">
-                <div className="calculator-grid" >
-                    <Display />
-                    <CalcButton>AC</CalcButton>
-                    <CalcButton>DEL</CalcButton>
-                    <CalcButton operation={'+'}>+</CalcButton>
-                    <CalcButton digit={'7'}></CalcButton>
-                    <CalcButton digit={'8'}></CalcButton>
-                    <CalcButton digit={'9'}></CalcButton>
-                    <CalcButton operation={'-'}>-</CalcButton>
-                    <CalcButton digit={'4'}></CalcButton>
-                    <CalcButton digit={'5'}></CalcButton>
-                    <CalcButton digit={'6'}></CalcButton>
-                    <CalcButton operation={'*'}>×</CalcButton>
-                    <CalcButton digit={'1'}></CalcButton>
-                    <CalcButton digit={'2'}></CalcButton>
-                    <CalcButton digit={'3'}></CalcButton>
-                    <CalcButton operation={'/'}>÷</CalcButton>
-                    <CalcButton>.</CalcButton>
-                    <CalcButton digit={'0'}></CalcButton>
-                    <CalcButton>=</CalcButton>
+                <div className="row-item">
+                    <ChatPrompt />
                 </div>
-            </div>
-            <div className="row-item">
-                <DropdownPrompts  />
-            </div>
-            <Info/>
+                <div className="row-item">
+                    <div className="calculator-grid" >
+                        <Display />
+                        <CalcButton>AC</CalcButton>
+                        <CalcButton>DEL</CalcButton>
+                        <CalcButton operation={'+'}>+</CalcButton>
+                        <CalcButton digit={'7'}></CalcButton>
+                        <CalcButton digit={'8'}></CalcButton>
+                        <CalcButton digit={'9'}></CalcButton>
+                        <CalcButton operation={'-'}>-</CalcButton>
+                        <CalcButton digit={'4'}></CalcButton>
+                        <CalcButton digit={'5'}></CalcButton>
+                        <CalcButton digit={'6'}></CalcButton>
+                        <CalcButton operation={'*'}>×</CalcButton>
+                        <CalcButton digit={'1'}></CalcButton>
+                        <CalcButton digit={'2'}></CalcButton>
+                        <CalcButton digit={'3'}></CalcButton>
+                        <CalcButton operation={'/'}>÷</CalcButton>
+                        <CalcButton>.</CalcButton>
+                        <CalcButton digit={'0'}></CalcButton>
+                        <CalcButton>=</CalcButton>
+                    </div>
+                </div>
+                <div className="row-item">
+                    <DropdownPrompts  />
+                </div>
+            <Info className='info-card'/>
         </div>
     );
 };
